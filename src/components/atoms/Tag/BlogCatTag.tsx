751548388b8d967155d95b87d3867bf06fd1d100
TowfMinh <vinxen2019@gmail.com>
2024-07-08 09:28:31 +0700

    fix merge bug

--- src/components/atoms/Tag/BlogCatTag.tsx
+++ src/components/atoms/Tag/BlogCatTag.tsx
@@ -13,23 +13,3 @@ const BlogCatTag = ({ tags }: Props) => {
     </>
   );
 };
-
-export default BlogCatTag;
-import { Button } from "antd";
-import { Blog } from "../../../types/Blog";
-
-type Props = {
-  tags?: [];
-};
-
-const BlogCatTag = ({ tags }: Props) => {
-  return (
-    <>
-      <div className="flex flex-wrap gap-2">
-        <Button>{tags}</Button>
-      </div>
-    </>
-  );
-};
-
-export default BlogCatTag;
--- src/components/molecules/blogcategory/BlogCategorieslist.tsx
+++ src/components/molecules/blogcategory/BlogCategorieslist.tsx
@@ -20,27 +20,3 @@ const BlogCategorieslist = () => {
     </div>
   );
 };
-export default BlogCategorieslist;
-const categories = [
-  { category: "Technology Trends and News" },
-  { category: "Gaming Insights" },
-  { category: "Security and Privacy" },
-  { category: "Tech Lifestyle and Productivity" },
-  { category: "Product Spotlight" },
-  { category: "How-to Guides and Tutorials" },
-  { category: "Buying Guides and Tips" },
-];
-
-const BlogCategorieslist = () => {
-  return (
-    <div className="mt-4">
-      <p className="font-medium text-xl">Categories</p>
-      <div className="mt-6 gap-4 flex flex-col">
-        {categories.map((category, index) => (
-          <div className="font-light text-xl">{category.category}</div>
-        ))}
-      </div>
-    </div>
-  );
-};
-export default BlogCategorieslist;
